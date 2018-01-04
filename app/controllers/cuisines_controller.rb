a4b504af51cb421d35408bcca6101e572effc42d
Thiago <talarini@gmail.com>
2018-01-04 18:09:39 -0200

    administrador cadastra cozinhas

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -3,4 +3,24 @@ class CuisinesController <ApplicationController
     @cuisines = Cuisine.find(params[:id])
     @recipes = Recipe.where(cuisine_id: params[:id])
   end
+
+  def new
+    @cuisine = Cuisine.new
+  end
+
+  def create
+    @cuisine = Cuisine.create(cuisine_params)
+    if @cuisine.save
+      redirect_to cuisine_path(@cuisine)
+    else
+      render '_error'
+    end
+  end
+
+  private
+
+  def cuisine_params
+    params.require(:cuisine).permit(:name)
+  end
+
 end
