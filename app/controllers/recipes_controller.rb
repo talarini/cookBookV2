1ffe335c8c3c66d71cdf6f791de6157627667fc1
Thiago <talarini@gmail.com>
2018-01-10 19:52:14 -0200

    Refatoraçao do controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,56 +1,56 @@
 class RecipesController < ApplicationController
+
+  before_action :get_recipes, only: [:edit, :update]
+  before_action :get_params, only: [:show, :create, :update, :new]
+
   def show
     @recipes = Recipe.find(params[:id])
-    @cuisines = Cuisine.all
-    @recipe_types = RecipeType.all
   end
 
   def new
     @recipe = Recipe.new
-    @cuisines = Cuisine.all
-    @recipe_types = RecipeType.all
   end
 
   def create
     @recipe = Recipe.create(recipe_params)
-    @cuisines = Cuisine.all
-    @recipe_types = RecipeType.all
-
     if @recipe.save
       redirect_to @recipe
     else
       flash.now[:error] = 'Você deve informar todos os dados da receita'
-      render 'new'
+      render :new
     end
   end
 
   def edit
-    @recipe = Recipe.find(params[:id])
   end
 
   def update
-    @recipe = Recipe.find(params[:id])
-    @cuisines = Cuisine.all
-    @recipe_types = RecipeType.all
     @recipe.update(recipe_params)
     if @recipe.save
       redirect_to recipe_path(@recipe)
     else
       flash.now[:error] = 'Você deve informar todos os dados da receita'
-      render 'new'
+      render :new
     end
   end
 
   def search
-    @recipe_search = params[:search]
-    @recipes = Recipe.where "title like ?", "%#{@recipe_search}%"
-
+    @recipes = Recipe.where "title like ?", params[:search]
   end
 
   private
 
   def recipe_params
-    params.require(:recipe).permit(:title, :recipe_type_id, :cuisine_id, :difficulty, :cook_time, :ingredients, :method)
+    params.require(:recipe).permit(:title, :recipe_type_id, :cuisine_id,
+                                   :difficulty, :cook_time, :ingredients, :method)
   end
 
+  def get_recipes
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def get_params
+    @cuisines = Cuisine.all
+    @recipe_types = RecipeType.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  resources :recipes, only: [:show, :new, :create, :edit, :update]
+  resources :recipes, only: [:show, :new, :create, :edit, :update] do
+    collection do
+      get 'search'
+    end
+  end
+  
   resources :cuisines, only: [:show, :new, :create]
   resources :recipe_types, only: [:show, :new, :create]
-
-  get "/search" => "recipes#search", as: :search_recipe
 
 end
