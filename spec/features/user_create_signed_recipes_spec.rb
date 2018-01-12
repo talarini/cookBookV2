5384f8d9505134eece202cb1024d554b4a1e0f6a
Thiago <talarini@gmail.com>
2018-01-12 15:10:30 -0200

    rspec para receitas de autor

---
+++ spec/features/user_create_signed_recipes_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+feature 'logged user create recipe' do
+  scenario 'successfully' do
+    user = User.create(email:'aaa@email.com', password:'123456')
+    cuisine = Cuisine.create(name: 'Arabe')
+    recType = RecipeType.create(name: 'Entrada')
+
+    visit root_path
+    click_on 'Entrar'
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    within('div.actions') do
+      click_on 'Entrar'
+    end
+    expect(page).to have_content("Bem Vindo #{user.email}")
+
+    click_on 'Enviar uma receita'
+
+    fill_in 'Título', with: 'Tabule'
+    select 'Arabe', from: 'Cozinha'
+    select 'Entrada', from: 'Tipo da Receita'
+    fill_in 'Dificuldade', with: 'Fácil'
+    fill_in 'Tempo de Preparo', with: '45'
+    fill_in 'Ingredientes', with: 'Trigo para quibe, cebola, tomate picado, azeite, salsinha'
+    fill_in 'Como Preparar', with: 'Misturar tudo e servir. Adicione limão a gosto.'
+    click_on 'Enviar'
+
+    expect(page).to have_css('h1', text: 'Tabule')
+    expect(page).to have_css('h3', text: 'Detalhes')
+    expect(page).to have_css('p', text: 'Entrada')
+    expect(page).to have_css('p', text: 'Arabe')
+    expect(page).to have_css('p', text: 'Fácil')
+    expect(page).to have_css('p', text: "45 minutos")
+    expect(page).to have_css('h3', text: 'Ingredientes')
+    expect(page).to have_css('p', text: 'Trigo para quibe, cebola, tomate picado, azeite, salsinha')
+    expect(page).to have_css('h3', text: 'Como Preparar')
+    expect(page).to have_css('p', text:  'Misturar tudo e servir. Adicione limão a gosto.')
+
+    expect(page).to have_content("Enviado por #{user.email}")
+
+  end
+end
