a82583613085f1271702ea847611d95a89798c23
Thiago <talarini@gmail.com>
2018-01-18 20:27:29 -0200

    Usuario envia foto da receita

--- spec/features/user_upload_recipe_image_spec.rb
+++ spec/features/user_upload_recipe_image_spec.rb
@@ -24,4 +24,28 @@ feature 'user upload recipe image' do
 
     expect(page).to have_css("img[src*='bolo.jpeg']")
   end
+
+  scenario 'and try to send a non image file' do
+    user = create(:user)
+    cuisine = create(:cuisine, name:'Arabe')
+    recipe_type = create(:recipe_type, name:'Entrada')
+
+    sign_in user
+
+    visit root_path
+    click_on 'Enviar uma receita'
+
+    fill_in 'Título', with: 'Tabule'
+    select 'Entrada', from: 'Tipo da Receita'
+    select 'Arabe', from: 'Cozinha'
+    fill_in 'Dificuldade', with: 'Fácil'
+    fill_in 'Tempo de Preparo', with: '45'
+    fill_in 'Ingredientes', with: 'Trigo para quibe, cebola, tomate picado, azeite, salsinha'
+    fill_in 'Como Preparar', with: 'Misturar tudo e servir. Adicione limão a gosto.'
+
+    page.attach_file('Foto do Prato', Rails.root + 'spec/support/texto.txt')
+    click_on 'Enviar'
+
+    expect(page).to have_content("Você deve informar todos os dados da receita")
+  end
 end
